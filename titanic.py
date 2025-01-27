6faab51899620ad2eed4d5293f2297a6fa324a10
700N645 <700n645@gmail.com>
2025-01-27 17:22:06 -0300

    Cambio de dato a tipo float64 para la columna edad

--- titanic.py
+++ titanic.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('datasets/titanic3.csv', dtype={'age':np.int32})
+data = pd.read_csv('datasets/titanic3.csv', dtype={'age':np.float64})
 
 print(data.head())
