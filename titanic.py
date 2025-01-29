1d02982a9b634be5f57bdd578ec2821aaad4ee90
700N645 <700n645@gmail.com>
2025-01-29 02:24:49 -0300

    Sustitucion de valores Nan y agregado del archivo .gitignore

--- titanic.py
+++ titanic.py
@@ -4,4 +4,13 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('datasets/titanic3.csv', dtype={'age':np.float64})
 
-print(data.head())
+df = data
+#Reemplazo de la columana vacias
+var = pd.isnull(df['age']).values.ravel().sum() #263 datos nulosñ en la columna 'age'
+df['age'] = df['age'].fillna(df['age'].mean())
+df['body'] = df['body'].fillna(0)
+df['cabin'] = df['cabin'].fillna('Desconocido')
+df['boat'] = df['boat'].fillna('Desconocido')
+df['home.dest'] = df['home.dest'].fillna('Desconocido')
+
+print(df)
